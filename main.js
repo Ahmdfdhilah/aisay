f86647ef87a794bfd3aaa79a162a95ef0f21125b
AhmadFadillah <ahmad.121140173@student.itera.ac.id>
2023-05-13 01:49:20 +0700

    adding

---
+++ main.js
@@ -0,0 +1,11 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+var options = {
+  strings: ["Heiii kamuu nenek nenek!", "Nihh buat luu nii gua petikk di kebun raya iteraa", "AHAHAHAHA"],
+  typeSpeed: 40,
+  loop: false,
+  delaySpeed: 80
+};
+
+new Typed("#typed", options);
